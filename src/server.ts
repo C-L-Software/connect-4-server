e9e92e5d3634558005de86a31a6bec83272d2ac7
joeylemon <joeyclemon@gmail.com>
2023-08-03 21:59:07 -0400

    add linter

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-import * as express from "express";
-import * as http from "http";
-import * as socketio from "socket.io";
-import * as path from "path";
+import * as express from 'express'
+import * as http from 'http'
+import * as socketio from 'socket.io'
+import * as path from 'path'
 
-const app = express.default();
+const app = express.default()
 
-app.get("/", (_req, res) => {
-  res.send({ uptime: process.uptime() });
-});
+app.get('/', (_req, res) => {
+  res.send({ uptime: process.uptime() })
+})
 
 app.get('/test', (_req, res) => {
-    res.sendFile(path.resolve("./src/test_page/index.html"));
-});
+  res.sendFile(path.resolve('./src/test_page/index.html'))
+})
 
-const server = http.createServer(app);
-const io = new socketio.Server(server);
+const server = http.createServer(app)
+const io = new socketio.Server(server)
 
-io.on("connection", (...params) => {
-    console.log(params);
-});
+io.on('connection', (...params) => {
+  console.log(params)
+})
 
-io.on("message", (...params) => {
-    console.log(params);
-});
+io.on('message', (...params) => {
+  console.log(params)
+})
 
 server.listen(3000, () => {
-  console.log("Running at localhost:3000");
-});
+  console.log('Running at localhost:3000')
+})
