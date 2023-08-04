fe4e78d28df5ca587801ec9500fe3bff35010588
joeylemon <joeyclemon@gmail.com>
2023-08-03 21:55:27 -0400

    basic FE -> BE connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
-import express, {Express, Request, Response} from 'express';
+import * as express from "express";
+import * as http from "http";
+import * as socketio from "socket.io";
+import * as path from "path";
 
-const app: Express = express();
-const port = 3000;
+const app = express.default();
 
-app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello, this is Express + TypeScript');
+app.get("/", (_req, res) => {
+  res.send({ uptime: process.uptime() });
 });
 
-app.listen(port, ()=> {
-    console.log(`[Server]: I am running at https://localhost:${port}`);
+app.get('/test', (_req, res) => {
+    res.sendFile(path.resolve("./src/test_page/index.html"));
+});
+
+const server = http.createServer(app);
+const io = new socketio.Server(server);
+
+io.on("connection", (...params) => {
+    console.log(params);
+});
+
+io.on("message", (...params) => {
+    console.log(params);
+});
+
+server.listen(3000, () => {
+  console.log("Running at localhost:3000");
 });
